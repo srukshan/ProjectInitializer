fce7c0bf7e3208e5d4d6c9610829b3cedc18e5ef
Sachith Rukshan <sachithrukshanmail@gmail.com>
2019-10-19 14:24:03 +0000

    Storing Commandline Arguments to Config

--- helper/Options.py
+++ helper/Options.py
@@ -1,7 +1,7 @@
 #Important:-
 #  Page to Find details of how to do - https://www.tutorialspoint.com/python/python_command_line_arguments.htm
 #  Page to Find details of how to do - https://gist.github.com/AtmaMani/4cdda473f02d70ed26035a3f191d5b73
-
+from model.config import Config
 import argparse
 
 parser = argparse.ArgumentParser(description="Project Management Tool")
@@ -17,24 +17,32 @@ parser.add_argument('-v','--vcs', help='Version Control: \n1.Github', type=int)
 
 parser.add_argument('--path', help='Project Path')
 
-parser.add_argument('-s','--save', help='Save current Settings and Default')
+parser.add_argument('-s','--save', help='Save current Settings and Default', action='store_true')
 
 class Options:
     project_name = ""
     action=""
-    options = {}
     def __init__(self):
+        self.load()
         self.__args = parser.parse_args()
         self.setProjectName()
         self.setOptions()
+        if self.__args.save:
+            self.config.save()
 
-    #TODO:Get Project Name
     def setProjectName(self):
         self.project_name = self.__args.proj_name
 
     #TODO:Get Options
     def setOptions(self):
-        pass
+        self.action = ALTER if self.__args.alter else DELETE if self.__args.delete else CREATE
+
+
+    def load(self):
+        self.config = Config.load()
+
+    def save(self):
+        self.config.save()
 
 #TODO:Display Help Menu For Options
 def help():
